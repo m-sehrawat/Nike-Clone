a802dd5aeae213f2c103b3f5aaa2a1c58b6074ff
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-03-29 23:11:19 +0530

    basic structure done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <Heading>Testing</Heading>
+        </div>
+    );
+}
+
+export default App;
