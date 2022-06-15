b4030e5cbca86adf6de5ab6693820873b8cf0bf3
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-15 16:42:40 +0530

    login form data collected

--- client/src/components/auth/LoginForm.jsx
+++ client/src/components/auth/LoginForm.jsx
@@ -1,21 +1,35 @@
 import { Input, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { showResetPage } from "../../redux/features/auth/actions";
 
 export const LoginForm = () => {
 
     const dispatch = useDispatch();
+    const [form, setForm] = useState({ email: "", password: "" });
+
+
+    const handleInputChange = ({ target: { name, value } }) => {
+        setForm({...form, [name]: value});
+    };
+
+    const handleOnSubmit = (e)=>{
+        e.preventDefault();
+        
+        console.log(form);
+
+    }
 
     const displayReset = () => {
         dispatch(showResetPage());
-    }
+    };
 
     return (
         <>
-            <form>
+            <form onSubmit={handleOnSubmit}>
                 <VStack w={['95%', '85%']} mx={'auto'} gap={'8px'}>
-                    <Input type={'email'} placeholder="Email address" />
-                    <Input type={'password'} placeholder="Password" />
+                    <Input onChange={handleInputChange} name="email" type={'email'} placeholder="Email address" />
+                    <Input onChange={handleInputChange} name="password" type={'password'} placeholder="Password" />
                     <Text onClick={displayReset} _hover={{ textDecoration: "underline" }}
                         w={'100%'} color={'#b0a8af'} textAlign={'right'} my={'10px'} cursor={'pointer'}>
                         Forgot your password?
