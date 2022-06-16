b01e1a93f63637f81c663e436963f168a46965e3
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-16 18:20:57 +0530

    my account menu buttons styling added

--- client/src/components/auth/Logout.jsx
+++ client/src/components/auth/Logout.jsx
@@ -1,8 +1,8 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { Button, Divider, Flex, Icon, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { FiLogOut } from "react-icons/fi";
 import { FaRegHeart } from "react-icons/fa";
 import { BsCart2, BsFillCaretDownFill } from "react-icons/bs";
-import { RiLuggageCartLine,RiCoupon3Line } from "react-icons/ri";
+import { RiLuggageCartLine, RiCoupon3Line } from "react-icons/ri";
 
 
 export const Logout = () => {
@@ -11,22 +11,25 @@ export const Logout = () => {
         <>
             <Menu>
                 <MenuButton as={Button} size='sm' bg={'transparent'} rightIcon={<BsFillCaretDownFill />}>Mohit</MenuButton>
-                <MenuList>
-                     <MenuItem icon={<FaRegHeart />} >
-                        Wishlist
-                    </MenuItem>
-                   <MenuItem icon={<RiLuggageCartLine />} >
-                        Orders
-                    </MenuItem>
-                    <MenuItem icon={<RiCoupon3Line />} >
-                        Coupons
-                    </MenuItem>
-                    <MenuItem icon={<BsCart2 />} >
-                        Cart
-                    </MenuItem>
-                    <MenuItem icon={<FiLogOut />}>
-                       Logout
-                    </MenuItem>
+                <MenuList >
+                    <Flex flexDirection={'column'} gap={'5px'} fontSize={'17px'}>
+                        <MenuItem icon={<FaRegHeart />} >
+                            Wishlist
+                        </MenuItem>
+                        <MenuItem icon={<RiLuggageCartLine />} >
+                            Orders
+                        </MenuItem>
+                        <MenuItem icon={<RiCoupon3Line />} >
+                            Coupons
+                        </MenuItem>
+                        <MenuItem icon={<BsCart2 />} >
+                            Cart
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem icon={<FiLogOut />}>
+                            Logout
+                        </MenuItem>
+                    </Flex>
                 </MenuList>
             </Menu>
         </>
