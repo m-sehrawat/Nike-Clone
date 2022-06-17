558dc0a0e297257084361203c286c10e1f86c39a
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-17 11:24:35 +0530

    add some changes in public routes

--- client/src/routes/Public.jsx
+++ client/src/routes/Public.jsx
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
+
 export const Public = ({children}) => {
 
     const token = useSelector((state)=>state.authReducer.token);
-    console.log('token:', token)
 
     return token ? <Navigate to={'/'} /> : children;
 };
