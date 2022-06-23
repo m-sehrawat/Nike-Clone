3d62647f5b15454286df65e0e0bd9139d109128a
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-23 22:42:58 +0530

    favourite page setup done

--- client/src/pages/favourite/Favourite.jsx
+++ client/src/pages/favourite/Favourite.jsx
@@ -1,10 +1,14 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 export const Favourite = () => {
 
     return (
         <>
-            <Heading>Favourite</Heading>
+            <Box border={'1px solid red'} maxW={'1450px'} mx={'auto'} my={'20px'}>
+                <Text fontSize={'20px'} fontWeight={500}>Favourites</Text>
+
+
+            </Box>
         </>
     );
 };
