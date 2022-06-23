56a1abd4a06ce0b5b9234af17f5389b22fd8124e
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-23 23:00:39 +0530

    favourite model added

---
+++ server/src/models/favourite.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+
+const reqString = { type: String, required: true };
+const reqNumber = { type: Number, required: true };
+const reqArray = { type: Array, required: true }
+
+const favouriteSchema = new Schema({
+    title: reqString,
+    gender: reqString,
+    description: reqString,
+    category: reqString,
+    price: reqNumber,
+    size: reqArray,
+    color: reqString,
+    rating: reqNumber,
+    img: reqArray,
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+module.exports = model('favourite', favouriteSchema);
