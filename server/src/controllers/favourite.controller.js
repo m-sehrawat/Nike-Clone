e4bbd40b6b510b4c0f13eee60aa5674d9cbbd03e
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-24 00:20:25 +0530

    check duplicate favourite products middleware added

--- server/src/controllers/favourite.controller.js
+++ server/src/controllers/favourite.controller.js
@@ -1,10 +1,11 @@
 const authorization = require('../middlewares/authorization');
+const checkDuplicateFavourite = require('../middlewares/checkDuplicateFavourite');
 const Favourite = require('../models/favourite.model');
 const { postFavourite } = require('./crud.controller');
 const router = require('express').Router();
 
 
-router.post('/', authorization, postFavourite(Favourite));
+router.post('/', authorization, checkDuplicateFavourite, postFavourite(Favourite));
 
 
 module.exports = router;
