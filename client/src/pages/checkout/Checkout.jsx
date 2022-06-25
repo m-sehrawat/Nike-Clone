8fc4b18d2429722e80a741c8ebbe319d97e305c1
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-25 22:29:22 +0530

    checkout form data is coming

--- client/src/pages/checkout/Checkout.jsx
+++ client/src/pages/checkout/Checkout.jsx
@@ -1,8 +1,33 @@
 import { Box, Flex, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { ContinueBtn } from "./ContinueBtn";
 
 
 export const Checkout = () => {
+
+    const initState = {
+        firstName: "",
+        lastName: "",
+        addressLine1: "",
+        addressLine2: "",
+        locality: "",
+        postalCode: "",
+        state: "",
+        country: "",
+        email: "",
+        mobile: ""
+    };
+
+    const [form, setForm] = useState(initState);
+
+    const handleInputChange = ({ target: { name, value } }) => {
+        setForm({ ...form, [name]: value });
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(form);
+    }
 
 
 
@@ -20,23 +45,23 @@ export const Checkout = () => {
                     Enter your name and address:
                 </Text>
 
-                <form>
+                <form onSubmit={handleFormSubmit}>
                     <Flex flexDirection={'column'} gap={'20px'}>
-                        <Input type={'text'} name={'firstName'} placeholder={'First Name*'} />
-                        <Input type={'text'} name={'lastName'} placeholder={'Last Name*'} />
-                        <Input type={'text'} name={'addressLine1'} placeholder={'Address Line 1*'} />
-                        <Input type={'text'} name={'addressLine2'} placeholder={'Address Line 2'} />
+                        <Input onChange={handleInputChange} type={'text'} name={'firstName'} placeholder={'First Name*'} />
+                        <Input onChange={handleInputChange} type={'text'} name={'lastName'} placeholder={'Last Name*'} />
+                        <Input onChange={handleInputChange} type={'text'} name={'addressLine1'} placeholder={'Address Line 1*'} />
+                        <Input onChange={handleInputChange} type={'text'} name={'addressLine2'} placeholder={'Address Line 2'} />
                         <Flex gap={'20px'}>
-                            <Input type={'text'} name={'locality'} placeholder={'City/Locality*'} />
-                            <Input type={'text'} name={'postalCode'} placeholder={'Postal Code*'} />
+                            <Input onChange={handleInputChange} type={'text'} name={'locality'} placeholder={'City/Locality*'} />
+                            <Input onChange={handleInputChange} type={'number'} name={'postalCode'} placeholder={'Postal Code*'} />
                         </Flex>
                         <Flex gap={'20px'}>
-                            <Input type={'text'} name={'state'} placeholder={'State/Territory*'} />
-                            <Input type={'text'} name={'country'} placeholder={'Country*'} />
+                            <Input onChange={handleInputChange} type={'text'} name={'state'} placeholder={'State/Territory*'} />
+                            <Input onChange={handleInputChange} type={'text'} name={'country'} placeholder={'Country*'} />
                         </Flex>
                         <Text fontSize={'20px'} fontWeight={600} mt={'30px'}>What's your contact information?</Text>
-                        <Input type={'text'} name={'email'} placeholder={'Email*'} />
-                        <Input type={'text'} name={'mobile'} placeholder={'Mobile*'} />
+                        <Input onChange={handleInputChange} type={'email'} name={'email'} placeholder={'Email*'} />
+                        <Input onChange={handleInputChange} type={'number'} name={'mobile'} placeholder={'Mobile*'} />
 
                         <ContinueBtn />
                     </Flex>
