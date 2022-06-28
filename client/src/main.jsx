fb7d68fdc963de1c77d94e71a6a44e1b4121be7c
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-28 11:25:04 +0530

    heroku link added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,9 +10,9 @@ import { theme } from './theme';
 
 
 
-axios.defaults.baseURL = "http://localhost:8080";
+// axios.defaults.baseURL = "http://localhost:8080";
 
-// axios.defaults.baseURL = "https://mohit-nike-clone.herokuapp.com/";
+axios.defaults.baseURL = "https://mohit-nike-clone.herokuapp.com/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
