f8e20c259b8574c76f4ab1a122cc3927c5670c6a
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2024-01-21 17:06:02 +0530

    backend url updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,7 @@ import { theme } from './theme';
 
 // axios.defaults.baseURL = "http://localhost:8080";
 
-axios.defaults.baseURL = "https://mohit-nike-clone.herokuapp.com/";
+axios.defaults.baseURL = "https://nike-clone-backend-apcy.onrender.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
